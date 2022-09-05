85e8e31f11acc4e1615a93e189be54085347b89b
Aleksei Aliapkin <a.alyapkin@tais.ru>
2022-09-05 11:24:46 +0300

    add smart path copy

---
+++ README.md
@@ -0,0 +1,8 @@
+# smart-path-copier
+
+Copies path from src to *.js
+
+# install
+
+        npx vsce package   
+        code --install-extension smart-path-copier-0.0.1.vsix
---
+++ extension.js
@@ -0,0 +1,42 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const copyPaste = require("copy-paste");
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "smart-path-copier" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('smart-path-copier.smartCopyPath', function () {
+		// The code you place here will be executed every time your command is executed
+		let filename = vscode.window.activeTextEditor.document.fileName;
+		filename = filename.replace(/\\/g, '\/');
+		filename = filename.replace(/^.*(src)[\/\\]/, '');
+		filename = filename.replace(/\.js$/, '');
+		copyPaste.copy(filename, function() {
+			vscode.window.showInformationMessage('SmartCopyPath copied!');
+			return null;
+		})
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
